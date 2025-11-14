97b48a98fbf617d99ab9d4c871e2cd482d9380a4
datadiego <juandiegomariscal@gmail.com>
2025-11-14 15:11:14 +0100

    script para abrir terminales en el mismo directorio actual

--- alacritty-config.sh
+++
@@ -1,2 +0,0 @@
-sudo apt install alacritty -y
-sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,6 +1,4 @@
-mkdir ~/.config/i3
-mkdir ~/.config/alacritty
-
 cp ./dotfiles/i3 ~/.config/i3/config
 cp ./dotfiles/polybar-config.ini ~/.config/polybar/config.ini
 cp ./dotfiles/alacritty.toml ~/.config/alacritty/alacritty.toml
+cp ./dotfiles/new_terminal.sh ~/.config/i3/new_terminal.sh
