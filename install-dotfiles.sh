dc405662ee498d2b47745c95816bb2ca67458e69
datadiego <juandiegomariscal@gmail.com>
2025-11-14 13:48:16 +0100

    creamos directorios de configuracion para i3 y alacritty

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,3 +1,6 @@
+mkdir ~/.config/i3
+mkdir ~/.config/alacritty
+
 cp ./dotfiles/i3 ~/.config/i3/config
 cp ./dotfiles/polybar-config.ini ~/.config/polybar/config.ini
 cp ./dotfiles/alacritty.toml ~/.config/alacritty/alacritty.toml
