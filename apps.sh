e47ccf51597cf22a115cfafba964247ece7ab96e
datadiego <juandiegomariscal@gmail.com>
2025-11-15 17:58:03 +0100

    apps y pwas

--- apps.sh
+++ apps.sh
@@ -1,3 +1,7 @@
 sudo snap remove firefox;
 sudo snap install --classic code;
-sudo apt install -y chromium sqlite3 eza;
+sudo apt install -y chromium sqlite3 eza npm nodejs micro;
+
+sudo npm i -g opencode live-server;
+
+cp apps/chatgpt.desktop ~/.local/share/applications/
