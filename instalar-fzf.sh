d3a19120724a7c99548721fdf1984bd6772a573d
datadiego <juandiegomariscal@gmail.com>
2025-11-16 15:41:40 +0100

    unificamos scripts

--- instalar-fzf.sh
+++
@@ -1,3 +0,0 @@
-# instalar fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sh 0-install.sh
-sh 1-nerdfonts.sh
-sh 2-install-dotfiles.sh
-sh apps.sh
-./instalar-fzf.sh
